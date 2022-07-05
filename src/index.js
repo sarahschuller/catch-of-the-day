36e5cc0e4121bc5fbdcfad224cb611b6b5508f94
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-05 18:11:06 -0400

    import StorePicker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>Hello!</p>
-    }
-}
-
+render(<StorePicker />, document.querySelector('#main'));
