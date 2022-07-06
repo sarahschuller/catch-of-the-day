342797655695db97b10720f4d6a028dae7015db6
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-06 18:53:11 -0400

    create tagline props

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,17 @@ class Header extends React.Component {
     render() {
         return (
             <header className="top">
-                
+                <h1>Catch 
+                    <span className="ofThe">
+                        <span className="of">Of</span> 
+                        <span className="the">The</span>
+                    </span>
+                    Day</h1>
+                <h3 className="tagline">
+                <span>{this.props.tagline}</span>
+            </h3>
             </header>
+            
         )
     }
 }
