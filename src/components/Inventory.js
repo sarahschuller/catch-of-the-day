3e5cef4e544977a66515f8602b4bb5a4a6a3b8f5
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-06 18:53:00 -0400

    add placeholder text

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,7 +3,9 @@ import React from "react";
 class Inventory extends React.Component {
     render() {
         return (
-            
+            <div className="inventory">
+                <p>INVENTORY!!!</p>
+            </div>
         )
     }
 }
--- src/components/Order.js
+++ src/components/Order.js
@@ -3,7 +3,9 @@ import React from "react";
 class Order extends React.Component {
     render() {
         return (
-            
+            <div className="order">
+                <p>ORDER HERE!</p>
+            </div>
         )
     }
 }
