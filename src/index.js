a6b2c45822bfb981122bf78bc449e3421ae23710
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-07 20:41:34 -0400

    update router import to solve issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { render } from 'react-dom';
-// import { Router } from "react-router-dom";
-import App from "./components/App";
+import { render } from "react-dom";
+import Router from "./components/Router";
 import "./css/style.css";
 
-
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector("#main"));
