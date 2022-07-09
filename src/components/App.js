eca5891b38dcad42bfc1e44470a32f941e23918f
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-09 11:06:18 -0400

    update to remove redundant variable

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,9 @@ class App extends React.Component {
         // Add the new fish to the fishes variable
         fishes[`fish${Date.now()}`] = fish;
         // Set the new fishes object to state
-        this.setState({
-            fishes: fishes
-        });
+        this.setState({ fishes });
     };
-    
+
     render() {
         return (
             <div className="catch-of-the-day">
