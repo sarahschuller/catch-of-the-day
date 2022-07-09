74aac776c8382c03277cd76d7a74382658415dd1
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-09 16:25:54 -0400

    add index to fish

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,14 @@ class App extends React.Component {
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} add addToOrder = {this.addToOrder} />)}
+                        {Object.keys(this.state.fishes).map(key => 
+                        <Fish 
+                        key={key}
+                        index={key}
+                        details={this.state.fishes[key]} 
+                        addToOrder = {this.addToOrder} 
+                        />
+                        )}
                     </ul>
                 </div>
                 <Order />
