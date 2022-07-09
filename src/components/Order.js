47167170761ec7769bb6aaf13652f479a53cbd7e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-09 19:35:40 -0400

    update

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,28 +1,60 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
-    render() {
-        const orderIds = Object.keys(this.props.order);
-        const total = orderIds.reduce((prevTotal, key) => {
-            const fish = this.props.fishes[key];
-            const count = this.props.order[key];
-            const isAvailable = fish && fish.status === "available";
-            if(isAvailable) {
-                return prevTotal + (count * fish.price);
-            }
-            return prevTotal;
-        }, 0);
+  static propTypes = {
+    fishes: PropTypes.object,
+    order: PropTypes.object,
+    removeFromOrder: PropTypes.func
+  };
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === "available";
+    // Make sure the fish is loaded before we continue!
+    if (!fish) return null;
 
-        return (
-            <div className="order-wrap">
-                <h2>Order</h2>
-                {orderIds}
-                <div className="total">
-                    <strong>{total}</strong>
-                </div>
-            </div>
-        )
+    if (!isAvailable) {
+      return (
+          <li key={key}>
+            Sorry {fish ? fish.name : "fish"} is no longer available
+          </li>
+      );
     }
+    return (
+        <li key={key}>
+                <span>{count}</span>
+                lbs {fish.name}
+            {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.removeFromOrder(key)}>
+              &times;
+            </button>
+        </li>
+    );
+  };
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+          {orderIds.map(this.renderOrder)}
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Order;
