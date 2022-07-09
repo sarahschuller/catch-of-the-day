db038993e115ef9e2e36fefc482080d94d6088df
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-09 16:29:28 -0400

    spread everything from state into order

--- src/components/App.js
+++ src/components/App.js
@@ -48,9 +48,8 @@ class App extends React.Component {
                         )}
                     </ul>
                 </div>
-                <Order
-                fishes = {this.state.fishes}
-                order = {this.state.order}
+                <Order 
+                {...this.state}
                  />
                 <Inventory 
                 addFish={this.addFish} 
