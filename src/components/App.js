4550699c43d8fed4fbda6ad9e78c9a56e65ce783
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-08 19:35:56 -0400

    add context and sync to fishes state

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match;
-    this.ref = base.syncState(`${params.storeId}/fishes`);
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes',
+    });
   }
 
   addFish = fish => {
