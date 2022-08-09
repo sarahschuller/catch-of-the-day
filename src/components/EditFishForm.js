86143bafe7ebd0ad9a00ccb5e1af122e0b3a83dc
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-08 21:15:44 -0400

    correct spelling in note

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -16,7 +16,7 @@ class EditFishForm extends React.Component {
   handleChange = e => {
     // console.log(e.currentTarget.value);
     // update that fish
-    // 1. Take a copy of the curernt fish
+    // 1. Take a copy of the current fish
     const updatedFish = {
       ...this.props.fish,
       [e.currentTarget.name]:
