1ab0693158384b7318b388ed660d074b5c8fee24
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-08-12 19:15:23 -0400

    add buttons for logging in with Github and Twitter

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Login = () => (
+const Login = (props) => (
     <nav className="login">
         <h2>Inventory Login</h2>
+        <p>Sign in to manage your store's inventory.</p>
+        <button 
+        className="github" 
+        onClick={() => props.authenticate('Github')}>
+            Log in with Github
+        </button>
+
+        <button 
+        className="twitter" 
+        onClick={() => props.authenticate('Twitter')}>
+            Log in with Twitter
+        </button>
     </nav>
 );
+
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired
+};
 
 export default Login;
